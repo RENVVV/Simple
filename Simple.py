3dddc62ee6490a6c61f406f2e0d954bad7bdf872
RENVVV <89802496+RENVVV@users.noreply.github.com>
2022-09-01 18:17:39 +0700

    Simple.py

--- Simple.py
+++ Simple.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
        os.system("git pull")
    except Exception as e:
        exit(str(e))
-cetak(f"[bold green]Sorry, we closed the simple script, thank you in advance for those who have used this script, we have maximized so that this script is useful, but many users complain because many users say that this script has a bad script ")
+cetak(f"[bold green]Sorry, we closed the simple script, thank you in advance for those who have used this script, we have maximized so that this script is useful, because many users say that this script has a bad script ")

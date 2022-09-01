3c35e5cd2efe60634c51088b1140b0ab362c923c
RENVVV <89802496+RENVVV@users.noreply.github.com>
2022-09-01 18:15:36 +0700

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,8 @@
+from rich import print as cetak
+import os
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+   except Exception as e:
+       exit(str(e))
+cetak(f"[bold green]Sorry, we closed the simple script, thank you in advance for those who have used this script, we have maximized so that this script is useful, but many users complain because many users say that this script has a bad script ")
